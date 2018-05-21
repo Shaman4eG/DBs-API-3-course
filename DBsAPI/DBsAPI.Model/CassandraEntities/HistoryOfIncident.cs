2de64689088157d3bd193cc648388061f6d725d2
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2018-05-22 00:18:57 +0300

    Added Cassandra's models

---
+++ DBsAPI/DBsAPI.Model/CassandraEntities/HistoryOfIncident.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBsAPI.Model.CassandraEntities
+{
+    public class HistoryOfIncidient
+    {
+        public int TramID { get; set; }
+        public DateTimeOffset Day { get; set; }
+        public string Incidient { get; set; }
+        public string Description { get; set; }
+    }
+}
