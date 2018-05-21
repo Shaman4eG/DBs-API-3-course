d0fdfb0098e0d8aaf96c08057eb71f24ca62f5d7
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2018-05-21 23:41:54 +0300

    Added cassandra's models

---
+++ DBsAPI/DBsAPI.Model/CassandraEntities/HistoryOfActions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBsAPI.Model.CassandraEntities
+{
+    class HistoryOfActions
+    {
+        public int WorkerID { get; set; }
+        public int TramID { get; set; }
+        public string Date { get; set; }
+        public string Result { get; set; }
+        public string Comment { get; set; }
+    }
+}
