699aa9ef6a631232307099536b5da4cdaee79bd0
Vladbak <vladbakshen@mail.ru>
2018-05-23 21:55:03 +0400

    added Driver model

---
+++ DBsAPI/DBsAPI.Model/MongoDBEntities/Driver.cs
@@ -0,0 +1,13 @@
+﻿namespace DBsAPI.Model.MongoDBEntities
+{
+    using MongoDB.Bson;
+    public class Driver
+    {
+        public ObjectId _id{ get; set; }
+        public string name{ get; set; }
+        public int expirience { get; set; }
+        public int salary{ get; set; }
+        public string[] licensedformodels { get; set; }
+
+    }
+}
