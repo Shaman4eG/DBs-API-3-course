9898053a81b6f11bc0b332c19fd40495657828b6
Vladbak <vladbakshen@mail.ru>
2018-05-24 19:26:06 +0400

    new model, queries and controller for Trams

---
+++ DBsAPI/DBsAPI.API/Controllers/MongoDBControllers/TramsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using DBsAPI.DBsQueries.MongoDBQueries;
+using System.Threading.Tasks;
+using DBsAPI.Model.MongoDBEntities;
+using MongoDB.Bson;
+
+namespace DBsAPI.API.Controllers.MongoDBControllers
+{
+    [RoutePrefix("api/trams")]
+    public class TramsController : ApiController
+    {
+        private TramQueries tramQueries;
+        TramsController()
+        {
+            tramQueries = new TramQueries();
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Tram>> GetAll()
+        {
+            return tramQueries.ReadAllTrams();
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public Task<Tram> Get(string id)
+        {
+            return tramQueries.ReadTram(id);
+        }
+
+        [HttpPost]
+        public Task Post(int number, int route, string model, [FromBody] Tram.Capacity capacity)
+        {
+           return tramQueries.AddTram(new Tram() { number = number, route = route, model = model, capacity = capacity });
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public Task Put(string id, int? number, int? route, string model, [FromBody] Tram.Capacity capacity)
+        {
+            Task task = Task.Delay(0);
+
+            if (number != null)
+                task = tramQueries.UpdateTram(id, $"{nameof(number)}", number);
+
+            if (route != null)
+                task = tramQueries.UpdateTram(id, $"{nameof(route)}", route);
+
+            if (model != null)
+                task = tramQueries.UpdateTram(id, $"{nameof(model)}", model);
+
+            if (capacity != null)
+                task = tramQueries.UpdateTram(id, $"{nameof(capacity)}", capacity);
+
+            return task;
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public Task Delete(string id)
+        {
+            return tramQueries.RemoveTram(id);
+        }
+    }
+}
---
+++ DBsAPI/DBsAPI.DBsQueries/MongoDBQueries/TramQueries.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using DBsAPI.Helpers;
+using DBsAPI.Model.MongoDBEntities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace DBsAPI.DBsQueries.MongoDBQueries
+{
+    public class TramQueries
+    {
+        private readonly string uri = DBsConnetctionStrings.MongoDB.uri;
+
+        private MongoClient client;
+        private IMongoDatabase db;
+        private IMongoCollection<Tram> TramsCollection;
+
+        public TramQueries()
+        {
+            client = new MongoClient(uri);
+            db = client.GetDatabase("TramParkDB");
+            TramsCollection = db.GetCollection<Tram>("Trams");
+        }
+
+        public async Task<IEnumerable<Tram>> ReadAllTrams()
+        {
+            return await TramsCollection.Find(new BsonDocument()).ToListAsync();
+
+        }
+
+        public async Task<Tram> ReadTram(string id)
+        {
+            var filter = CreateFilterById(id);
+            return await TramsCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task AddTram(Tram tram)
+        {
+            await TramsCollection.InsertOneAsync(tram);
+        }
+
+        public async Task RemoveTram(string id)
+        {
+            var filter = CreateFilterById(id);
+            await TramsCollection.DeleteOneAsync(filter);
+        }
+
+        public async Task UpdateTram(string id, string fieldName, string fieldValue)
+        {
+            var filter = CreateFilterById(id);
+            var update = Builders<Tram>.Update.Set(fieldName, fieldValue);
+
+            await TramsCollection.UpdateOneAsync(filter, update);
+        }
+
+        public async Task UpdateTram(string id, string fieldName, int? fieldValue)
+        {
+            var filter = CreateFilterById(id);
+            var update = Builders<Tram>.Update.Set(fieldName, fieldValue);
+
+            await TramsCollection.UpdateOneAsync(filter, update);
+        }
+
+        public async Task UpdateTram(string id, string fieldName, object fieldValue)
+        {
+            var filter = CreateFilterById(id);
+            var update = Builders<Tram>.Update.Set(fieldName, fieldValue);
+
+            await TramsCollection.UpdateOneAsync(filter, update);
+        }
+
+        private FilterDefinition<Tram> CreateFilterById(string id)
+        {
+            var queryId = new ObjectId(id);
+            return Builders<Tram>.Filter.Eq($"{nameof(Tram._id)}", queryId);
+        }
+    }
+}
